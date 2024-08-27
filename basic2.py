8711d9c2250c724a3421a3ae8c4f390eca0020d6
iasror <iasror@gmail.com>
2024-08-27 09:43:25 +0700

    cek

--- basic2.py
+++ basic2.py
@@ -2,15 +2,25 @@ import cv2
 import mediapipe as mp
 import csv
 
-def write_landmarks_to_csv(landmarks, frame_number, csv_data):
+def write_landmarks_to_csv(landmarks, frame_number, csv_data, shape):
     print(f"Landmark coordinates for frame {frame_number}:")
+    #for normalize pose landmark
+    image_height, image_width, _ = shape
     for idx, landmark in enumerate(landmarks):
+        #print(f"{mp_pose.PoseLandmark(idx).name}: (x: {landmark.x*image_width}, y: {landmark.y*image_height}, z: {landmark.z*image_width})")
         print(f"{mp_pose.PoseLandmark(idx).name}: (x: {landmark.x}, y: {landmark.y}, z: {landmark.z})")
-        csv_data.append([frame_number, mp_pose.PoseLandmark(idx).name, landmark.x, landmark.y, landmark.z])
+        #if mp_pose.PoseLandmark(idx).name == 'NOSE' :
+            #print(f"{mp_pose.PoseLandmark(idx).name}: (x: {landmark.x}, y: {landmark.y}, z: {landmark.z})")
+        csv_data.append([frame_number, mp_pose.PoseLandmark(idx).name, landmark.x*image_width, landmark.y*image_height, landmark.z*image_width])
     print("\n")
 
-video_path = 'input_videos/input_video.mp4'
-output_csv = 'output/output_video_fed.csv'
+
+#video_path = 'input_videos/input_video.mp4'
+#output_csv = 'output/output_video.csv'
+
+video_path = 'input_videos/input_video_fed_bnf.mp4'
+output_csv = 'output/bnf/output_video.csv'
+
 
 # Initialize MediaPipe Pose and Drawing utilities
 mp_pose = mp.solutions.pose
@@ -35,11 +45,16 @@ while cap.isOpened():
     result = pose.process(frame_rgb)
 
     # Draw the pose landmarks on the frame
+    # capture from 165-190 : 25 frame.
     if result.pose_landmarks:
         mp_drawing.draw_landmarks(frame, result.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+        #mp_drawing.plot_landmarks(result.pose_world_landmarks, mp_pose.POSE_CONNECTIONS)
+        cv2.putText(frame, str(frame_number), (70, 50), cv2.FONT_HERSHEY_PLAIN, 4,
+                    (255, 255, 0), 3)
+        cv2.imwrite("output_img/frame%d.jpg" % frame_number, frame)
 
         # Add the landmark coordinates to the list and print them
-        write_landmarks_to_csv(result.pose_landmarks.landmark, frame_number, csv_data)
+        write_landmarks_to_csv(result.pose_landmarks.landmark, frame_number, csv_data, frame_rgb.shape)
 
     # Display the frame
     cv2.imshow('MediaPipe Pose', frame)
