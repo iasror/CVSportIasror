7427fdf7b8d155916f3a390db5d8d2a78505a976
iasror <iasror@gmail.com>
2024-09-03 10:24:00 +0700

    cek

--- basic2.py
+++ basic2.py
@@ -9,8 +9,8 @@ def write_landmarks_to_csv(landmarks, frame_number, csv_data, data_dist, dist2, 
     #for normalize pose landmark
     image_height, image_width, _ = shape
     for idx, landmark in enumerate(landmarks):
-        #print(f"{mp_pose.PoseLandmark(idx).name}: (x: {landmark.x*image_width}, y: {landmark.y*image_height}, z: {landmark.z*image_width})")
-        print(f"{mp_pose.PoseLandmark(idx).name}: (x: {landmark.x}, y: {landmark.y}, z: {landmark.z})")
+        print(f"{mp_pose.PoseLandmark(idx).name}: (x: {landmark.x*image_width}, y: {landmark.y*image_height}, z: {landmark.z*image_width})")
+        #print(f"{mp_pose.PoseLandmark(idx).name}: (x: {landmark.x}, y: {landmark.y}, z: {landmark.z})")
         #if mp_pose.PoseLandmark(idx).name == 'NOSE' :
             #print(f"{mp_pose.PoseLandmark(idx).name}: (x: {landmark.x}, y: {landmark.y}, z: {landmark.z})")
         csv_data.append([frame_number, mp_pose.PoseLandmark(idx).name, landmark.x*image_width, landmark.y*image_height, landmark.z*image_width])
